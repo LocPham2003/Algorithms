60a950c7d55eaa002faf0c17c6a2eba12f186cf2
lochasathanoscar <phamducloc2003@gmail.com>
2020-04-26 10:17:40 -0400

    algorithm is semi working run into infinite loop if there are two identical numbers in the list

--- Sortings/Merge sorting.py
+++ Sortings/Merge sorting.py
@@ -1,35 +1,62 @@
 def sort(array, odd):
     inOrder = False
-    totalSortTimes = 2
     randomNumber = [0]
-    print(array)
     if odd:
         array.append(randomNumber)
 
     while not inOrder:
-        k = 0
+        isFirstArrayNotEmpty = True
+        isSecondArrayNotEmpty = True
         tempArr = []
-        while array[0][0].__len__() != 0 and array[1][0].__len__() != 0:
-            if array[0][0] > array[1][0]:
+        while isFirstArrayNotEmpty and isSecondArrayNotEmpty:
+            print("Sorting")
+            isFirstArrayNotEmpty = len(array[0]) != 0
+            isSecondArrayNotEmpty = len(array[1]) != 0
+            if len(array[0]) == 0 and len(array[1]) == 1:
                 tempArr.append(array[1][0])
-            else:
+                del array[1][0]
+            elif len(array[0]) == 1 and len(array[1]) == 0:
                 tempArr.append(array[0][0])
-
-
-        while k < 2:
+                del array[0][0]
+            elif len(array[0]) == 0 and len(array[1]) > 1:
+                if array[1][0] > array[1][1]:
+                    tempArr.append(array[1][1])
+                    del array[1][1]
+                else:
+                    tempArr.append(array[1][0])
+                    del array[1][0]
+            elif len(array[1]) == 0 and len(array[0]) > 1:
+                if array[0][0] > array[0][1]:
+                    tempArr.append(array[0][1])
+                    del array[0][1]
+                else:
+                    tempArr.append(array[0][0])
+                    del array[0][0]
+            elif array[0][0] > array[1][0]:
+                tempArr.append(array[1][0])
+                del array[1][0]
+            elif array[0][0] < array[1][0]:
+                tempArr.append(array[0][0])
+                del array[0][0]
+            
+        array.append(tempArr)
+        if len(array) == 2:
             del array[0]
-            k += 1
+        elif len(array) > 2:
+            del array[0]
+            del array[0]
 
-        if array.__len__() == 1:
+        if len(array) == 1:
             inOrder = True
-    totalSortTimes *= 2
+    print(array)
+    return array
 
 
 def split(array):
     odd = False
     tempList = array.split(" ")
 
-    if tempList.__len__() % 2 == 1:
+    if len(tempList) % 2 == 1:
         odd = True
 
     splitList = []
@@ -37,8 +64,10 @@ def split(array):
         singleton = [int(i)]
         splitList.append(singleton)
 
-    sort(splitList, odd)
+    return sort(splitList, odd)[0]
 
 
 inputList = input()
-split(inputList)
+sortList = split(inputList)
+print(sortList)
+
